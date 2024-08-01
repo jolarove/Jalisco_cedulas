a825f3d362c70dd0d787bbded9fd0b06fb6806aa
Lauro Rodríguez <laurorodven@gmail.com>
2024-08-01 15:18:30 -0600

    Actualización 01/08/24

--- descarga_cedulas.py
+++ descarga_cedulas.py
@@ -361,7 +361,7 @@ time.sleep(3)
 #Damos clic en botón conectar
 btnConectar = driver.find_element(by='id', value='connectButton')
 btnConectar.click()
-time.sleep(10)
+time.sleep(30)
 try:
     #Si aparece el cuadro de diálogo de cookies, lo manejamos
     btnCookies = driver.find_element(by='xpath', value='//div[@class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]')
